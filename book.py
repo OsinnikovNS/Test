27dfdd7b61c7093c9850461b23e1c0f46ba74d7a
OsinnikovNS <osinnikov_ns@mail.ru>
2024-04-08 22:01:51 +0500

    исправлена ошибка (пробел)

--- book.py
+++ book.py
@@ -1,22 +1,24 @@
 class Backpack:
     """Рюкзак"""
-    def __init__ (self, gift=None):
+
+    def __init__(self, gift=None):
         self.content = []
         if gift:
             self.content.append(gift)
-    def __str__ (self):
+
+    def __str__(self):
         return 'Рюкзак: ' + ','.join(self.content)
 
-
-    def __eq__ (self, other):
+    def __eq__(self, other):
         return self.content == other.content
+
 
 my_backpack = Backpack(gift='бутерброд, хлеб, масло')
 san_backpack = Backpack(gift='бутерброд, хлеб')
 print(my_backpack)
 print(san_backpack)
 
-if  san_backpack == my_backpack:
+if san_backpack == my_backpack:
     print('соответствие')
 else:
     print('не соответствие')
